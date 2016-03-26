cdc195afab408cf75f03663cfc2239082d2004ff
Jin Park <jin+github@jinpark.net>
2016-03-26 18:17:52 +0900

    use tabcapture

--- content.js
+++ content.js
@@ -1,51 +1,33 @@
-var script = document.createElement('script');
-var js = [
-"audioTagQ = {};",
-"audioTagQNextKey = 0;",
-"var wrappedCreateElement = document.createElement;",
-"document.createElement = function createElement(name) {",
-"    var res = wrappedCreateElement.call(this, name);",
-"    if (name === 'audio') {",
-"        console.log(res);",
-"        res.crossOrigin = 'anonymous';",
-"        document.body.appendChild(res);",
-"        audioTagQ[audioTagQNextKey++] = {elem: res, checked: 0};",
-"        if (res.id == 'html5AudioObject_Single'){ console.log(['here!', res]) }",
-"    }",
-"    return res;",
-"};"
-];
-script.text = js.join("\n");
-script.dataset.origin = 'soundcloudrepeat';
-document.documentElement.appendChild(script);
 
-setTimeout(function() {
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var source = audioCtx.createMediaElementSource(document.querySelectorAll('audio')[1]);
-    console.log(document.querySelectorAll('audio')[1]);
-    console.log(source);
-    var analyser = audioCtx.createAnalyser();
-    source.connect(analyser);
-    analyser.connect(audioCtx.destination);
+// var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// chrome.tabCapture.capture({
+//             audio : true,
+//             video : false
+//         }, function(stream) {
+//             console.log('stream', stream);
+//             var source = audioCtx.createMediaElementSource(stream);
+//             console.log(source);
+//             var analyser = audioCtx.createAnalyser();
+//             source.connect(analyser);
+//             analyser.connect(audioCtx.destination);
 
-    analyser.fftSize = 2048;
-    var bufferLength = analyser.frequencyBinCount;
-    var dataArray = new Uint8Array(bufferLength);
+//             analyser.fftSize = 2048;
+//             var bufferLength = analyser.frequencyBinCount;
+//             var dataArray = new Uint8Array(bufferLength);
 
-    analyser.getByteTimeDomainData(dataArray);
+//             analyser.getByteTimeDomainData(dataArray);
 
-    var WIDTH = 300;
-    var HEIGHT = 150;
+//             var WIDTH = 300;
+//             var HEIGHT = 150;
 
-    function draw() {
-        analyser.getByteTimeDomainData(dataArray);
-        port.postMessage({arr: dataArray, bufferLength: analyser.frequencyBinCount});
-    };
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        port = chrome.runtime.connect({name: "visualizer"});
-        var intv = setInterval(function(){draw()}, 1000 / 30);
-        port.onDisconnect.addListener(function(){clearInterval(intv)});
-    });
+//             function draw() {
+//                 analyser.getByteTimeDomainData(dataArray);
+//                 port.postMessage({arr: dataArray, bufferLength: analyser.frequencyBinCount});
+//             };
+//             chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//                 port = chrome.runtime.connect({name: "visualizer"});
+//                 var intv = setInterval(function(){draw()}, 1000 / 30);
+//                 port.onDisconnect.addListener(function(){clearInterval(intv)});
+//             });
 
-
-}, 10000 )
+//         });
--- popup.js
+++ popup.js
@@ -4,8 +4,12 @@ canvasCtx = canvas.getContext("2d");
 var WIDTH = 300;
 var HEIGHT = 300;
 
-chrome.runtime.onConnect.addListener(function(port) {
+var port = chrome.extension.connect({name: "Sample Communication"});
+port.postMessage("Hi BackGround");
+
+// chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
+    console.log(['inpopup', msg]);
     dataArray = msg.arr;
     bufferLength = msg.bufferLength;
     canvasCtx.fillStyle = 'rgb(200, 200, 200)';
@@ -33,10 +37,11 @@ chrome.runtime.onConnect.addListener(function(port) {
     canvasCtx.lineTo(canvas.width, canvas.height/2);
     canvasCtx.stroke();
   });
-});
+// });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {start: true}, function(response) {
-    console.log('sent initial');
-  });
-});
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   chrome.tabs.sendMessage(tabs[0].id, {start: true}, function(response) {
+//     console.log('sent initial');
+//   });
+// });
+
