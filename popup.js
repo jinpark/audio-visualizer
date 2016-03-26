edda3c99e36b29911d2ae05c5c32c65a6db8e8be
Jin Park <jin+github@jinpark.net>
2016-03-27 01:57:55 +0900

    clean up code, add new icon, add packed extension

--- popup.js
+++ popup.js
@@ -4,44 +4,34 @@ canvasCtx = canvas.getContext("2d");
 var WIDTH = 300;
 var HEIGHT = 300;
 
-var port = chrome.extension.connect({name: "Sample Communication"});
-port.postMessage("Hi BackGround");
+var port = chrome.extension.connect();
+port.postMessage({action: 'start'});
 
-// chrome.extension.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    console.log(['inpopup', msg]);
-    dataArray = msg.arr;
-    bufferLength = msg.bufferLength;
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+port.onMessage.addListener(function(msg) {
+  var dataArray = msg.data;
+  var bufferLength = msg.bufferLength;
 
-    canvasCtx.beginPath();
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
+  canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+  canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
 
-    for(var i = 0; i < bufferLength; i++) {
-       
-        var v = dataArray[i] / 128.0;
-        var y = v * HEIGHT/2;
+  canvasCtx.beginPath();
+  var sliceWidth = WIDTH * 1.0 / bufferLength;
+  var x = 0;
 
-        if(i === 0) {
-          canvasCtx.moveTo(x, y);
-        } else {
-          canvasCtx.lineTo(x, y);
-        }
+  for(var i = 0; i < bufferLength; i++) {
+      var v = dataArray[i] / 128.0;
+      var y = v * HEIGHT/2;
 
-        x += sliceWidth;
-    }
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-  });
-// });
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
 
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   chrome.tabs.sendMessage(tabs[0].id, {start: true}, function(response) {
-//     console.log('sent initial');
-//   });
-// });
-
+      x += sliceWidth;
+  }
+  canvasCtx.lineTo(canvas.width, canvas.height/2);
+  canvasCtx.stroke();
+});
